0597da402e80a0ada08d6eeb4df9d0a9a919ebff
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2016-05-01 06:47:04 -0400

    Update GoDoc badge

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Package `split` brings [split-complex numbers](https://en.wikipedia.org/wiki/Split-complex_number) to Go. It borrows heavily from the `math`, `math/cmplx`, and `math/big` packages.
 
-[![Go Report Card](https://goreportcard.com/badge/gojp/goreportcard)](https://goreportcard.com/report/github.com/meirizarrygelpi/split) [![GoDoc](https://godoc.org/github.com/golang/gddo?status.svg)](https://godoc.org/github.com/meirizarrygelpi/split)
+[![Go Report Card](https://goreportcard.com/badge/gojp/goreportcard)](https://goreportcard.com/report/github.com/meirizarrygelpi/split) [![GoDoc](https://godoc.org/github.com/meirizarrygelpi/split?status.svg)](https://godoc.org/github.com/meirizarrygelpi/split)
 
 ## To Do
 
--- complex_test.go
+++ complex_test.go
@@ -11,13 +11,46 @@ var (
 	s    = &Complex{0, 1}
 )
 
+func TestString(t *testing.T) {
+	var tests = []struct {
+		z    *Complex
+		want string
+	}{
+		{zero, "(0+0s)"},
+		{one, "(1+0s)"},
+		{s, "(0+1s)"},
+	}
+	for _, test := range tests {
+		if got := test.z.String(); got != test.want {
+			t.Errorf("String(%v) = %v, want %v",
+				test.z, got, test.want)
+		}
+	}
+}
+
 func TestEquals(t *testing.T) {}
 
 func TestCopy(t *testing.T) {}
 
-func TestString(t *testing.T) {}
+func ExampleNew() {
+	fmt.Println(New(1, 0))
+	fmt.Println(New(0, -1))
+	fmt.Println(New(3, -1))
+	fmt.Println(New(5, 7))
+	// Output:
+	// (1+0s)
+	// (0-1s)
+	// (3-1s)
+	// (5+7s)
+}
 
-func TestNew(t *testing.T) {}
+func TestIsInf(t *testing.T) {}
+
+func TestInf(t *testing.T) {}
+
+func TestIsNaN(t *testing.T) {}
+
+func TestNaN(t *testing.T) {}
 
 func TestScal(t *testing.T) {}
 
@@ -51,20 +84,12 @@ func TestQuad(t *testing.T) {
 
 func TestIsZeroDiv(t *testing.T) {}
 
+func TestInv(t *testing.T) {}
+
 func TestQuo(t *testing.T) {}
 
-func ExampleNew() {
-	fmt.Println(New(1, 0))
-	fmt.Println(New(0, -1))
-	fmt.Println(New(3, -1))
-	fmt.Println(New(5, 7))
-	// Output:
-	// (1+0s)
-	// (0-1s)
-	// (3-1s)
-	// (5+7s)
-}
+func TestIsIndempotent(t *testing.T) {}
 
-func TestRect(t *testing.T) {}
+func ExampleRect() {}
 
 func TestCurv(t *testing.T) {}
